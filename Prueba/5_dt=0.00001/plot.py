f2c1e3b255f92551414d2f428138fa38ee01cad9
rldromina <rldromina@gmail.com>
2019-12-19 11:34:45 -0300

    tu vieja

---
+++ Prueba/5_dt=0.00001/plot.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('test.txt');
+data3 = np.loadtxt('mediciones.txt');
+
+E_cin = data3[:,0];
+E_pot = data3[:,1];
+E_mec = data3[:,2];
+t = data3[:,3];
+
+plt.figure(1)
+plt.plot(t,2*E_cin/3,'-ok',t, E_mec, 'r')
+plt.figure(2)
+#plt.title('Temperatura', fontsize=16)
+plt.plot(t, data[:,0], 'ok',t, data[:,1]+1, 'or')
+plt.show()
+
+# data = np.loadtxt('mediciones.txt');
+# rho = data[:,0];
+# n = len(rho);
+# T = data[:,1];
+# p = data[:,2];
+# sigma_p = data[:,3];
+
+# plt.figure(1)
+# plt.title(r'N=343, T=1.1',fontsize=16)
+# plt.plot(rho,p,'-ok')
+# plt.xlabel(r'$\rho$',fontsize=16)
+# plt.xticks(fontsize=16)
+# plt.ylabel('P',fontsize=16)
+# plt.yticks(fontsize=16)
+# plt.figure(2)
+# plt.title(r'N=343, T=1.1',fontsize=16)
+# plt.xlabel(r'$\rho$',fontsize=16)
+# plt.xticks(fontsize=16)
+# plt.ylabel(r'$\sigma_P = \sqrt{\langle P^2\rangle - \langle P \rangle^2}$',fontsize=16)
+# plt.yticks(fontsize=16)
+# plt.plot(rho,sigma_p,'ok')
+# plt.show()
